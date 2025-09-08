2d344c8180593d2c1699ab659e3e4605b702a19c
Furkan Yılmaz <furkanyilmaz@192.168.1.43>
2025-09-08 12:53:01 +0300

    vercelconf

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: "/",
   define: {
     __API_BASE_URL__: JSON.stringify(
-      mode === 'development'
-        ? '/api'
+      mode === 'development'  
+      ? '/api'
         : (process.env.VITE_API_BASE_URL || 'http://45.87.120.218:8080/api')
     )
   },
   server: {
     proxy: {
-      '/api': {
+      '/api': { 
         target: 'http://45.87.120.218:8080',
         changeOrigin: true,
         secure: false
